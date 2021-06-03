create train.py. there is a bug that feedforward has 2 softmax.

src/AutoModel.py
@@ -1,45 +1,35 @@
 # import
 import torch.nn as nn
-from typing import Any
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # class
 
 
-class AutoModelTokenizer:
-    def __init__(self) -> None:
-        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-
-    def __call__(self, text, *args: Any, **kwds: Any):
-        return self.tokenizer(text, padding='max_length', max_length=128, return_tensors='pt')
-
-
 class AutoModel(nn.Module):
     def __init__(self):
         super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
         self.backbone_model = AutoModelForSequenceClassification.from_pretrained(
             'bert-base-cased')
         self.backbone_model.classifier = nn.Linear(
-            in_features=self.backbone_model.classifier.in_features, out_features=10)
+            in_features=self.backbone_model.classifier.in_features, out_features=2)
 
-    def forward(self, *args: Any, **kwds: Any):
-        return self.backbone_model(*args, **kwds)
+    def forward(self, x):
+        x = self.tokenizer(list(x), padding='max_length',
+                           max_length=128, return_tensors='pt')
+        return self.backbone_model(**x).logits
 
 
 if __name__ == '__main__':
     # create model
     model = AutoModel()
 
-    # create tokenizer
-    tokenizer = AutoModelTokenizer()
-
     # create input
-    x = tokenizer(text='Hello World!')
+    x = ['Hello World!']*10
 
     # get model output
-    y = model(**x)
+    y = model(x)
 
     # display the dimension of input and output
-    for k in x.keys():
-        print(x[k].shape)
-    print(y)
+    print(len(x))
+    print(y.shape)

src/model.py
@@ -10,7 +10,10 @@ import pandas as pd
 import numpy as np
 from src.utils import CreateTransformersSequenceModel, load_checkpoint, load_yaml, get_class_from_file
 import torch.optim as optim
-import transformers
+import os
+
+# system variables
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # def
 
@@ -85,6 +88,8 @@ class Net(LightningModule):
     def __init__(self, project_parameters):
         super().__init__()
         self.project_parameters = project_parameters
+        self.tokenizer = None if '.py' in project_parameters.backbone_model else CreateTransformersSequenceModel(
+        ).create_tokenizer(model_id=project_parameters.backbone_model)
         self.backbone_model = _get_backbone_model(
             project_parameters=project_parameters)
         self.activation_function = nn.Softmax(dim=-1)
@@ -95,7 +100,12 @@ class Net(LightningModule):
             num_classes=project_parameters.num_classes)
 
     def forward(self, x):
-        return self.activation_function(self.backbone_model(**x).logits)
+        if self.tokenizer is not None:
+            x = self.tokenizer(list(x), padding='max_length', truncation=True,
+                               max_length=self.project_parameters.max_length, return_tensors="pt")
+            return self.activation_function(self.backbone_model(**x).logits)
+        else:
+            return self.activation_function(self.backbone_model(x))
 
     def get_progress_bar_dict(self):
         # don't show the loss value
@@ -126,7 +136,8 @@ class Net(LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.backbone_model(**x).logits
+        #y_hat = self.backbone_model(**x).logits
+        y_hat = self.forward(x)
         loss = self.loss_function(y_hat, y)
         train_step_accuracy = self.accuracy(y_hat, y)
         return {'loss': loss, 'accuracy': train_step_accuracy}
@@ -188,15 +199,11 @@ if __name__ == '__main__':
     model.summarize()
 
     # create input data
-    tokenizer = get_class_from_file(filepath=project_parameters.backbone_model, class_name=project_parameters.tokenizer) if '.py' in project_parameters.backbone_model else CreateTransformersSequenceModel(
-    ).create_tokenizer(model_id=project_parameters.backbone_model)
-    x = tokenizer('Hello World!', padding='max_length', truncation=True,
-                  max_length=project_parameters.max_length, return_tensors="pt")
+    x = ['Hello World!']*10
 
     # get model output
-    y = model.forward(x)
+    y = model(x)
 
     # display the dimension of input and output
-    print(x['input_ids'].shape)
-    print(x['attention_mask'].shape)
+    print(len(x))
     print(y.shape)